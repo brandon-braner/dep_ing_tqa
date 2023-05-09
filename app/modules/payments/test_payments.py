7dfbc1ae3678eadca620b0093127aecd854614e5
Brandon Braner <brandon.braner@gmail.com>
2023-05-08 22:40:16 -0500

    testing_complete

--- app/modules/payments/test_payments.py
+++ app/modules/payments/test_payments.py
@@ -1,10 +1,28 @@
 import unittest
 from fastapi.testclient import TestClient
 from app.main import app
+from unittest.mock import Mock, patch
 
 client = TestClient(app)
+
 
 class TestPayments(unittest.TestCase):
     def test_get_payment(self):
         response = client.get("/payments")
-        self.assertEqual(response.status_code, 200)
+        resp_json = response.json()
+
+        assert resp_json == {'payments': [[123, 321, 1]]}
+
+    @patch("app.modules.payments.routes.get_db_conn")
+    def test_get_payment_mock(self, mock_get_db_conn):
+
+        expected_result = [[123123123, 1321, 10]]
+        mock_cursor = Mock()
+        mock_cursor.execute.return_value = None
+        mock_cursor.fetchall.return_value = expected_result
+        mock_get_db_conn.return_value = [None, mock_cursor]
+        mock_get_db_conn.execute.return_value = None
+        response = client.get("/payments")
+        resp_json = response.json()
+
+        assert resp_json == {'payments': expected_result}
